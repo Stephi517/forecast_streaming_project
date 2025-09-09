Fixed mistake in wind calculation

transform_functions.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import cartopy.crs as ccrs
 import geoviews as gv
+import holoviews as hv
 
 def add_wind_components(ds, u_name=None, v_name=None, ws_name=None, wd_name=None):
     #adds wind speed and direction 
@@ -16,17 +17,18 @@ def add_wind_components(ds, u_name=None, v_name=None, ws_name=None, wd_name=None
         u = ds[u_name]
         v = ds[v_name]
         ds["ws"] = xr.ufuncs.sqrt(u**2 + v**2)
-        ds["wd"] = (xr.ufuncs.degrees(xr.ufuncs.arctan2(u, v)) + 360) % 360  
+        # wd -> meteorological "coming from" 0° = North, 180° = South
+        ds["wd"] = (xr.ufuncs.degrees(xr.ufuncs.arctan2(u, v)) + 180) % 360  
         ds["u"] = u
         ds["v"] = v
     # adds u and v component 
     elif ws_name and wd_name:
         ws = ds[ws_name]
-        wd = xr.ufuncs.radians(ds[wd_name])
-        ds["u"] = -ws * xr.ufuncs.sin(wd)   
-        ds["v"] = -ws * xr.ufuncs.cos(wd)
+        wd = ds[wd_name]
+        ds["u"] = -ws * xr.ufuncs.sin(xr.ufuncs.radians(wd))   
+        ds["v"] = -ws * xr.ufuncs.cos(xr.ufuncs.radians(wd))
         ds["ws"] = ws
-        ds["wd"] = ds[wd_name]
+        ds["wd"] = wd
 
     return ds
 
@@ -55,6 +57,40 @@ def transform_MEPS(data):
     ds = ds.swap_dims({'valid_time':'step'})
     return ds 
 
+def make_wind_quiver(ds, step, stride=15):
+    ds = ds.sel(step=step)
+
+    w_speed = ds.ws.values[::stride, ::stride]
+    wd_deg = ds.wd.values[::stride, ::stride]
+    # Convert meteorological wind direction to "to-direction" for arrows and radiants for quiver
+    wd_rad = xr.ufuncs.radians((270 - wd_deg) % 360) 
+
+    lons = ds['longitude'].values
+    lats = ds['latitude'].values
+
+    if lons.ndim == 1 and lats.ndim == 1:
+        # ECMWF data lat and lon are 1D coords
+        lon, lat = np.meshgrid(lons[::stride], lats[::stride])
+    else:
+        # MEPS data lat lon (x,y) 2D
+        lon = lons[::stride, ::stride]
+        lat = lats[::stride, ::stride]
+
+    data = np.column_stack([lon.ravel(), lat.ravel(), wd_rad.ravel(), w_speed.ravel()])
+    
+    vectors = hv.VectorField(data, kdims=['longitude', 'latitude'],
+                            vdims=['wd_rad', 'w_speed']).opts(
+                                magnitude='w_speed',
+                                color="black",
+                                scale=0.8,
+                                #alpha=0.7,
+                                #projection=ccrs.PlateCarree(),
+                                #normalize=False,
+                                # scale=1.5,
+                                )
+    return vectors
+
+"""
 def make_wind_quiver(ds, step, xcoord, ycoord, stride=20):
     u = ds["u"].sel(step=step).values
     v = ds["v"].sel(step=step).values
@@ -101,3 +137,4 @@ def make_wind_quiver(ds, step, xcoord, ycoord, stride=20):
         scale=1.5,
     )
     return vectors
+"""